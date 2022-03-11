2a5c495b80483720c11704470dfd0833e12a4ed7
PushSalsa <100038729+PushSalsa@users.noreply.github.com>
2022-03-10 22:05:59 -0500

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,3 +1,15 @@
+
+
+Skip to content
+Using Gmail with screen readers
+Meet
+Hangouts
+Conversations
+14.17 GB of 15 GB (94%) used
+Terms · Privacy · Program Policies
+Last account activity: 2 hours ago
+Details
+
 #ifndef HEADER_END
 #define HEADER_END
 
@@ -10,14 +22,17 @@ typedef struct
 	int max;
 	int dmg;
 	int nrg;
+	int nrgActions[6];
+	char nameActions[6][20];
 } player;
 
-int life(char t);
-int att(char t);
-int nrg(char t);
-void wprompt(int l);
-void dprompt(int l);
-void bprompt(int l);
+void playerStats(player *play);
+int movement(char move);
+void prompt(int l,char type);
 int enemyStats (player *vile, int d, int xp);
+void battle (player *vile, player *play, int randomNum,char act);
 
 #endif
+
+functions.h.txt
+Displaying functions.c.txt.
